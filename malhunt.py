b7db51ef5b1b2fde363588d520ce091ec88459d6
Andrea Fortuna <andrea@andreafortuna.org>
2018-07-07 16:15:29 +0200

    webshell search test

--- malhunt.py
+++ malhunt.py
@@ -1,28 +1,44 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import os,shutil,sys
+import os,shutil,sys,time
 
 
-global MALUNTHOME
-MALUNTHOME = os.path.expanduser("~/.malhunt")
+global MALHUNTHOME
+MALHUNTHOME = os.path.expanduser("~/.malhunt")
 
 
 def clean_up():
-	shutil.rmtree("./rules", ignore_errors=True)
+	shutil.rmtree(MALHUNTHOME + "/rules", ignore_errors=True)
+	if os.path.isfile(MALHUNTHOME + '/malware_rules.yar'):
+		st=os.stat(MALHUNTHOME + "/malware_rules.yar")
+		max_age = time.time() - (60 * 60 * 24) #One day 
+		mtime=st.st_mtime
+		if mtime < max_age:
+			os.unlink(MALHUNTHOME + "/malware_rules.yar")
 
 def get_rules_from_git():
 	clean_up()
-	os.system("git clone https://github.com/Yara-Rules/rules.git")
+	os.system("git clone https://github.com/Yara-Rules/rules.git " + MALHUNTHOME + "/rules")
 
 def list_yara_files():
 	all_yara_files = []
-	for root, directories, filenames in os.walk("./rules/malware"):
+	for root, directories, filenames in os.walk(MALHUNTHOME + "/rules/malware"):
 		filenames.sort()
 		for file_name in filenames:
 			rule_filename, rule_file_extension = os.path.splitext(file_name)
 			if rule_file_extension == ".yar" or rule_file_extension == ".yara":
 				all_yara_files.append(os.path.join(root, file_name))
+	# BETA - Search for webshells
+        for root, directories, filenames in os.walk(MALHUNTHOME + "/rules/Webshells"):
+                filenames.sort()
+                for file_name in filenames:
+                        rule_filename, rule_file_extension = os.path.splitext(file_name)
+                        if rule_file_extension == ".yar" or rule_file_extension == ".yara":
+                                all_yara_files.append(os.path.join(root, file_name))
+
+
+
 	return all_yara_files
 
 def remove_incompatible_imports(files):
@@ -55,12 +71,12 @@ def fix_duplicated_rules(files):
 	return filtered_files
 
 def merge_rules(all_rules):
-	with open("malware_rules.yar", 'w') as fd:
+	with open(MALHUNTHOME + "/malware_rules.yar", 'w') as fd:
 		fd.write(''.join(all_rules))
 
 def image_identification(filename):
-	if os.path.isfile(MALUNTHOME + "/" + os.path.basename(filename) + ".imageinfo"):
-		with open(MALUNTHOME + "/" + os.path.basename(filename) + ".imageinfo",'r') as f:
+	if os.path.isfile(MALHUNTHOME + "/" + os.path.basename(filename) + ".imageinfo"):
+		with open(MALHUNTHOME + "/" + os.path.basename(filename) + ".imageinfo",'r') as f:
     			output = f.read()
 	 		return output
 	volimageInfo = os.popen("volatility -f " + filename +  " imageinfo  2>/dev/null | grep \"Suggested Profile(s)\" | awk '{print $4 $5 $6}'").read()
@@ -70,17 +86,17 @@ def image_identification(filename):
 		profileCheck =  os.popen("volatility -f " + filename +  " --profile=" + volProfile + " pslist 2>/dev/null").read()
 		print "	Check profile \033[1m" + volProfile + "\033[0m"
 		if "Offset" in profileCheck:
-			with open(MALUNTHOME + "/" + os.path.basename(filename) + ".imageinfo", 'w') as f:
+			with open(MALHUNTHOME + "/" + os.path.basename(filename) + ".imageinfo", 'w') as f:
 				f.write(volProfile)
 			return volProfile
 	return ""
 
 def yarascan(filename, volProfile):
-	if os.path.isfile(os.path.basename(filename) + '.malware_search.txt'):
-		with open(os.path.basename(filename) + '.malware_search.txt','r') as f:
+	if os.path.isfile(os.path.basename(filename) + '.malware_search'):
+		with open(os.path.basename(filename) + '.malware_search','r') as f:
                         volOutput = f.read()
 	else:
- 		volOutput = os.popen("volatility -f " + filename +  " yarascan --profile=" + volProfile + " -y malware_rules.yar  2>/dev/null").read()
+ 		volOutput = os.popen("volatility -f " + filename +  " yarascan --profile=" + volProfile + " -y " + os.path.expanduser("~/.malhunt") +  "/malware_rules.yar  2>/dev/null").read()
 	report = []
 	linereport = ""
 	for line in volOutput.splitlines():
@@ -92,7 +108,7 @@ def yarascan(filename, volProfile):
 			if (not (linereport in report)) and (not ("Str_Win32_" in linereport.lstrip())):
 				report.append(linereport)
 			linereport = ""
-	with open(os.path.basename(filename) + '.malware_search.txt', 'w') as f:
+	with open(os.path.basename(filename) + '.malware_search', 'w') as f:
 		f.write(volOutput)
 	return report
 
@@ -116,8 +132,8 @@ def banner_usage():
 	print "	" + sys.argv[0] + " imagefile"
 
 def check_env():
-	if  not os.path.exists(MALUNTHOME):
-		os.makedirs(MALUNTHOME)
+	if  not os.path.exists(MALHUNTHOME):
+		os.makedirs(MALHUNTHOME)
 
 def main():
 	banner_logo()
@@ -126,25 +142,28 @@ def main():
 		banner_usage()
 		return ""
 	imageFile = sys.argv[1]
-	print "\033[1mStep1 - \033[0mUpdate malware yara rules..."
-	get_rules_from_git()
-	all_yara_files = list_yara_files()
-	all_yara_filtered_1 = remove_incompatible_imports(all_yara_files)
-	all_yara_filtered_2 = fix_duplicated_rules(all_yara_filtered_1)
-	merge_rules(all_yara_filtered_2)
 	clean_up()
-	print "\033[1mStep2 - \033[0mStarting image identification for file \033[4m" + imageFile + "\033[0m..."
+	if not os.path.isfile(MALHUNTHOME + '/malware_rules.yar'):
+		print "\033[1m* \033[0mUpdate malware yara rules..."
+		get_rules_from_git()
+		all_yara_files = list_yara_files()
+		all_yara_filtered_1 = remove_incompatible_imports(all_yara_files)
+		all_yara_filtered_2 = fix_duplicated_rules(all_yara_filtered_1)
+		merge_rules(all_yara_filtered_2)
+	else:
+		print "\033[1m* \033[0mUsing cached yara rules..."
+	print "\033[1m** \033[0mStarting image identification for file \033[4m" + imageFile + "\033[0m..."
 	volProfile = image_identification(imageFile)
 	if (volProfile ==""):
 		print "Image identification failed!"
 		return ""
 	print "Image \033[4m" + imageFile + "\033[0m identified as \033[1m" + volProfile + "\033[0m"
-	print  "\033[1mStep3 - \033[0m Starting malware artifacts search..."
+	print  "\033[1m*** \033[0mStarting malware artifacts search..."
 	scanresult = yarascan(imageFile, volProfile)
 	if (len(scanresult) > 0):
-		print "\033[41m*** Found artifacts ***\033[0m"
+		print "\033[41m**** Found artifacts ****\033[0m"
 		print(''.join(scanresult))
-		print "Full scan results saved in \033[4m" + os.path.basename(imageFile) + ".malware_search.txt\033[0m"
+		print "Full scan results saved in \033[4m" + os.path.basename(imageFile) + ".malware_search\033[0m"
 	else:
 		print "\033[92mNo artifacts found!\033[0m"
 
